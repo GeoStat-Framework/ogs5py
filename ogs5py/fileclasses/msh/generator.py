some codestyle, bugfix, MSH.combine --> apply a shift, to prevent numerical errors

ogs5py/fileclasses/msh/generator.py
@@ -22,10 +22,12 @@ from ogs5py.fileclasses.msh.gmsh import (
 )
 
 
-def rectengular(dim=3,
-                mesh_origin=(0., 0., 0.),
-                element_no=(10, 10, 10),
-                element_size=(1., 1., 1.)):
+def rectengular(
+    dim=3,
+    mesh_origin=(0., 0., 0.),
+    element_no=(10, 10, 10),
+    element_size=(1., 1., 1.),
+):
     '''
     generate a rectengular grid in 2D or 3D
 
@@ -127,11 +129,13 @@ def rectengular(dim=3,
     return out
 
 
-def radial(dim=3,
-           mesh_origin=(0., 0., 0.),
-           angles=16,
-           rad=np.arange(11),
-           z_arr=-np.arange(2)):
+def radial(
+    dim=3,
+    mesh_origin=(0., 0., 0.),
+    angles=16,
+    rad=np.arange(11),
+    z_arr=-np.arange(2),
+):
     '''
     generate a radial grid in 2D or 3D
 
@@ -287,16 +291,18 @@ def radial(dim=3,
     return out
 
 
-def grid_adapter2D(out_dim=(100., 100.),
-                   in_dim=(50., 50.),
-                   out_res=(10., 10.),
-                   in_res=(1., 1.),
-                   out_pos=(0., 0.),
-                   in_pos=(25., 25.),
-                   z_pos=0.,
-                   in_mat=0,
-                   out_mat=0,
-                   fill=False):
+def grid_adapter2D(
+    out_dim=(100., 100.),
+    in_dim=(50., 50.),
+    out_res=(10., 10.),
+    in_res=(1., 1.),
+    out_pos=(0., 0.),
+    in_pos=(25., 25.),
+    z_pos=0.,
+    in_mat=0,
+    out_mat=0,
+    fill=False,
+):
     '''
     generate a grid adapter in 2D from an outer grid resolution
     to an inner grid resolution with gmsh.
@@ -369,17 +375,19 @@ def grid_adapter2D(out_dim=(100., 100.),
     return out
 
 
-def grid_adapter3D(out_dim=(100., 100.),
-                   in_dim=(50., 50.),
-                   z_dim=-10.,
-                   out_res=(10., 10., 10.),
-                   in_res=(5., 5., 5.),
-                   out_pos=(0., 0.),
-                   in_pos=(25., 25.),
-                   z_pos=0.,
-                   in_mat=0,
-                   out_mat=0,
-                   fill=False):
+def grid_adapter3D(
+    out_dim=(100., 100.),
+    in_dim=(50., 50.),
+    z_dim=-10.,
+    out_res=(10., 10., 10.),
+    in_res=(5., 5., 5.),
+    out_pos=(0., 0.),
+    in_pos=(25., 25.),
+    z_pos=0.,
+    in_mat=0,
+    out_mat=0,
+    fill=False,
+):
     '''
     generate a grid adapter in 3D from an outer grid resolution
     to an inner grid resolution with gmsh.
@@ -451,9 +459,11 @@ def grid_adapter3D(out_dim=(100., 100.),
     return out
 
 
-def block_adapter3D(xy_dim=10.,
-                    z_dim=5.,
-                    in_res=1.):
+def block_adapter3D(
+    xy_dim=10.,
+    z_dim=5.,
+    in_res=1.,
+):
     '''
     generate a block adapter that has a given resolution at the
     southern side with gmsh.

ogs5py/fileclasses/msh/tools.py
@@ -716,7 +716,7 @@ def remove_dim(mesh, remove):
         mesh["element_id"] = gen_std_elem_id(mesh["elements"])
 
 
-def combine(mesh_1, mesh_2, decimals=3):
+def combine(mesh_1, mesh_2, decimals=4, fast=False):
     '''
     Combine mesh_1 and mesh_2 to one single mesh. The node list will be
     updated to eliminate duplicates. Element intersections are not checked.
@@ -746,6 +746,10 @@ def combine(mesh_1, mesh_2, decimals=3):
         Number of decimal places to round the nodes to (default: 3).
         This will not round the output, it is just for comparison of the
         node vectors.
+    fast : bool, optional
+        If fast is True, the vector comparison is executed by a
+        decimal comparison. If fast is False, all pairwise distances
+        are calculated. Default: False
 
     Returns
     -------
@@ -763,10 +767,17 @@ def combine(mesh_1, mesh_2, decimals=3):
             element_id : dictionary
                 contains element ids for each element sorted by element types
     '''
+    # hack to prevent numerical errors from decimal rounding (random shift)
+    shift = np.random.rand(3)
+    shift_mesh(mesh_1, shift)
+    shift_mesh(mesh_2, shift)
     # combine the node lists and make them unique
-    nodes, __, ixr = unique_rows(np.vstack((mesh_1["nodes"],
-                                            mesh_2["nodes"])),
-                                 decimals=decimals)
+    nodes, __, ixr = unique_rows(
+        np.vstack((mesh_1["nodes"],
+                   mesh_2["nodes"])),
+        decimals=decimals,
+        fast=fast,
+    )
     node_id_repl = range(len(ixr))
     node_offset = mesh_1["nodes"].shape[0]
 
@@ -803,6 +814,11 @@ def combine(mesh_1, mesh_2, decimals=3):
            "elements": elements,
            "material_id": material_id,
            "element_id": element_id}
+
+    # back shifting of the meshes
+    shift_mesh(out, -shift)
+    shift_mesh(mesh_1, -shift)
+    shift_mesh(mesh_2, -shift)
 
     return out
 