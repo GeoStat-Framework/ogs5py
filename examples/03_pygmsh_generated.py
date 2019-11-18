0760cc56722c976ec9117c617fc3cf6d26ae5759
MuellerSeb <mueller.seb@posteo.de>
2019-11-18 15:25:24 +0100

    add pygmsh example

--- examples/03_pygmsh_generated.py
+++ examples/03_pygmsh_generated.py
@@ -4,17 +4,18 @@ from ogs5py import OGS
 import pygmsh
 
 geom = pygmsh.built_in.Geometry()
-poly = geom.add_polygon([
-    [ 0.0,  0.5, 0.0],
-    [-0.1,  0.1, 0.0],
-    [-0.5,  0.0, 0.0],
-    [-0.1, -0.1, 0.0],
-    [ 0.0, -0.5, 0.0],
-    [ 0.1, -0.1, 0.0],
-    [ 0.5,  0.0, 0.0],
-    [ 0.1,  0.1, 0.0]
+poly = geom.add_polygon(
+    [
+        [0.0, 0.5, 0.0],
+        [-0.1, 0.1, 0.0],
+        [-0.5, 0.0, 0.0],
+        [-0.1, -0.1, 0.0],
+        [0.0, -0.5, 0.0],
+        [0.1, -0.1, 0.0],
+        [0.5, 0.0, 0.0],
+        [0.1, 0.1, 0.0],
     ],
-    lcar=0.05
+    lcar=0.05,
 )
 axis = [0, 0, 1]
 geom.extrude(
@@ -22,18 +23,18 @@ geom.extrude(
     translation_axis=axis,
     rotation_axis=axis,
     point_on_axis=[0, 0, 0],
-    angle=2.0 / 6.0 * np.pi
+    angle=2.0 / 6.0 * np.pi,
 )
 model = OGS()
 # generate example above
 model.msh.generate("gmsh", geo_object=geom)
 model.msh.show()
-# generate a grid adapter in 2D
+# generate a predefined grid adapter in 2D
 model.msh.generate("grid_adapter2D", in_mat=1, out_mat=0, fill=True)
 model.msh.show(show_material_id=True)
-# generate a grid adapter in 3D
+# generate a predefined grid adapter in 3D
 model.msh.generate("grid_adapter3D", in_mat=1, out_mat=0, fill=True)
 model.msh.show(show_material_id=True)
-# generate a block adapter in 3D
+# generate a predefined block adapter in 3D
 model.msh.generate("block_adapter3D", xy_dim=5.0, z_dim=1.0, in_res=1)
 model.msh.show(show_element_id=True)
