fa26698d228a920fb9af3acb7979288b5ff1b385
MuellerSeb <mueller.seb@posteo.de>
2019-11-18 15:24:42 +0100

    Update example 1

--- examples/01_pump_test.py
+++ examples/01_pump_test.py
@@ -1,28 +1,31 @@
 # -*- coding: utf-8 -*-
-from ogs5py import OGS
 import anaflow as ana
+from ogs5py import OGS, specialrange, generate_time
 from matplotlib import pyplot as plt
 
+# discretization and parameters
+time = specialrange(0, 3600, 50, typ="cub")
+rad = specialrange(0, 1000, 100, typ="cub")
+obs = rad[21]
+angles = 32
+storage = 1e-3
+transmissivity = 1e-4
+rate = -1e-3
+# model setup
 model = OGS(task_root="pump_test", task_id="model")
-
-# generate a radial mesh
-model.msh.generate("radial", dim=2, rad=range(51))
-# generate a radial outer boundary
-model.gli.generate("radial", dim=2, rad_out=50.0)
+model.pcs.add_block(  # set the process type
+    PCS_TYPE="GROUNDWATER_FLOW", NUM_TYPE="NEW"
+)
+# generate a radial mesh and geometry ("boundary" polyline)
+model.msh.generate("radial", dim=2, rad=rad, angles=angles)
+model.gli.generate("radial", dim=2, rad_out=rad[-1], angles=angles)
 model.gli.add_points([0.0, 0.0, 0.0], "pwell")
-model.gli.add_points([1.0, 0.0, 0.0], "owell")
-
+model.gli.add_points([obs, 0.0, 0.0], "owell")
 model.bc.add_block(  # boundary condition
     PCS_TYPE="GROUNDWATER_FLOW",
     PRIMARY_VARIABLE="HEAD",
     GEO_TYPE=["POLYLINE", "boundary"],
     DIS_TYPE=["CONSTANT", 0.0],
-)
-model.st.add_block(  # source term
-    PCS_TYPE="GROUNDWATER_FLOW",
-    PRIMARY_VARIABLE="HEAD",
-    GEO_TYPE=["POINT", "pwell"],
-    DIS_TYPE=["CONSTANT_NEUMANN", -1.0e-04],
 )
 model.ic.add_block(  # initial condition
     PCS_TYPE="GROUNDWATER_FLOW",
@@ -30,44 +33,45 @@ model.ic.add_block(  # initial condition
     GEO_TYPE="DOMAIN",
     DIS_TYPE=["CONSTANT", 0.0],
 )
+model.st.add_block(  # source term
+    PCS_TYPE="GROUNDWATER_FLOW",
+    PRIMARY_VARIABLE="HEAD",
+    GEO_TYPE=["POINT", "pwell"],
+    DIS_TYPE=["CONSTANT_NEUMANN", rate],
+)
 model.mmp.add_block(  # medium properties
     GEOMETRY_DIMENSION=2,
-    STORAGE=[1, 1.0e-04],
-    PERMEABILITY_TENSOR=["ISOTROPIC", 1.0e-4],
-    POROSITY=0.2,
+    STORAGE=[1, storage],
+    PERMEABILITY_TENSOR=["ISOTROPIC", transmissivity],
 )
 model.num.add_block(  # numerical solver
     PCS_TYPE="GROUNDWATER_FLOW",
-    LINEAR_SOLVER=[2, 5, 1.0e-14, 1000, 1.0, 100, 4],
+    LINEAR_SOLVER=[2, 5, 1e-14, 1000, 1.0, 100, 4]
 )
 model.out.add_block(  # point observation
     PCS_TYPE="GROUNDWATER_FLOW",
     NOD_VALUES="HEAD",
     GEO_TYPE=["POINT", "owell"],
     DAT_TYPE="TECPLOT",
-    TIM_TYPE=["STEPS", 1],
-)
-model.pcs.add_block(  # set the process type
-    PCS_TYPE="GROUNDWATER_FLOW", NUM_TYPE="NEW"
 )
 model.tim.add_block(  # set the timesteps
     PCS_TYPE="GROUNDWATER_FLOW",
-    TIME_START=0,
-    TIME_END=600,
-    TIME_STEPS=[[12, 5], [18, 30]],
+    **generate_time(time)  # generate input from time-series
 )
 model.write_input()
 success = model.run_model()
-print("success", success)
+print("success:", success)
 # observation
 point = model.readtec_point(pcs="GROUNDWATER_FLOW")
 time = point["owell"]["TIME"]
 head = point["owell"]["HEAD"]
 # analytical solution
-head_ana = ana.theis(time, 1.0, 1e-4, 1e-4, rate=-1e-4)
+head_ana = ana.theis(time, obs, storage, transmissivity, rate=rate)
 # comparisson plot
-plt.plot(time, head, label="observations at r=1")
+plt.scatter(time, head, color="k", label="simulated, r={:04.2f}m".format(obs))
 plt.plot(time, head_ana, label="analytical solution")
+plt.xscale("symlog", linthreshx=10, subsx=range(1, 10))
+plt.xlim([0, 1.1 * time[-1]])
 plt.xlabel("time in s")
 plt.ylabel("head in m")
 plt.legend()
